12392e8bff91dab330f3086327200eca7b7edb11
Atsuyko <julien.jouvedeguibert@hotmail.fr>
2023-11-22 15:09:15 +0100

    Add Opinion and form on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Opinion;
+use App\Form\OpinionType;
+use App\Repository\OpeningTimeRepository;
+use App\Repository\OpinionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(OpinionRepository $opinionRepository, Request $request, EntityManagerInterface $em, OpeningTimeRepository $openingTimeRepository): Response
     {
+        $opinions = $opinionRepository->findAll();
+
+        $newOpinion = new Opinion();
+        $newOpinion->setIsValidate(false);
+
+        $form = $this->createForm(OpinionType::class, $newOpinion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newOpinion = $form->getData();
+
+            $em->persist($newOpinion);
+            $em->flush();
+
+            $this->addFlash(
+                'primary',
+                'Votre avis à bien été enrisgitré, il sera visible prochainement'
+            );
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'openingTimes' => $openingTimeRepository->findAll(),
+            'opinions' => $opinions,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/OpinionType.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Opinion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class OpinionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'form-label mt-3'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-3'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('score', ChoiceType::class, [
+                'expanded' => true,
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+                'choice_attr' => function () {
+                    return [
+                        'style' => 'margin-left: 10px; margin-right: 2px;',
+                        'class' => 'form-check-input'
+                    ];
+                },
+                'label' => 'Note',
+                'label_attr' => [
+                    'class' => 'form-label mt-3'
+                ],
+                'multiple' => false,
+                'required' => true,
+            ])
+            ->add('opinion', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Avis',
+                'label_attr' => [
+                    'class' => 'form-label mt-3'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-outline-primary mt-3 mb-3'
+                ],
+                'label' => 'Envoyer'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Opinion::class,
+        ]);
+    }
+}
